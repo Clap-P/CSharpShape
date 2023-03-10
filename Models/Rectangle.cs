fc203ca4e64e5fb3b09e99838999ddb918014b19
PierreC <pierre.clapisson@viacesi.fr>
2023-03-10 11:23:32 +0100

    first

---
+++ Models/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShape.Models
+{
+    public class Rectangle {
+
+        public double height { get; set; }
+        public double width { get; set; }
+
+
+        }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿ // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using CSharpShape.Models;
+using System.Drawing;
+
+
+class Program
+{
+    public double TotalArea(object[] arrObjects)
+    {
+
+        double area = 0;
+        Rectangle objRectangle;
+        Circle objCircle;
+        foreach(var obj in arrObjects)
+        {
+            if(obj is Rectangle)
+            {
+                area += obj.Height * obj.Width;
+            }
+            else
+            {
+                objCircle = (Circle)obj;
+                area += objCircle.Radius * objCircle.Radius * Math.PI;
+            }
+        }
+        return area;
+    }
+}
